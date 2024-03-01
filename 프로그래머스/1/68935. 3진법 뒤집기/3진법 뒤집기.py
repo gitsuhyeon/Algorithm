19410ebb41036309577f63413f04df2f64a227d2
gitsuhyeon <140365503+gitsuhyeon@users.noreply.github.com>
2024-03-01 21:35:45 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,18 +1,7 @@
 def solution(n):
-    answer = 0
-    temp = []
-    i = 3
-    while i <= n:
-        i = i * 3
-    if i > n:
-        i = i / 3
-    t = 0
+    answer = ""
     while n > 0:
-        l = n // i
-        answer += l * pow(3,t)
-        print(answer)
-        n = n % i
-        i = i / 3 
-        t += 1
+        n, rest = divmod(n,3)
+        answer += str(rest)
 
-    return answer
+    return int(answer,3)
