a95f91ee42a4608cc8428c51210c44cc133a5c01
gitsuhyeon <140365503+gitsuhyeon@users.noreply.github.com>
2024-03-08 15:13:53 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = ''
+    temp = s.split()
+    temp = list(map(int,temp))
+    answer += str(min(temp))
+    answer += ' '
+    answer += str(max(temp))
+    return answer
